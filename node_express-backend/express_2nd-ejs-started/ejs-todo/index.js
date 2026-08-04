5dea5a5e6e67dea1d3c64387a895c7aa777aef87
hassnainali914 <hassnainali4510123933235@gmail.com>
2026-08-04 23:27:05 +0500

    Vercel Compitable EJS Todo with Checkbox in Express

--- node_express-backend/express_2nd-ejs-started/ejs-todo/index.js
+++ node_express-backend/express_2nd-ejs-started/ejs-todo/index.js
@@ -1,26 +1,30 @@
 const express = require("express");
+const path = require("path");
 const dotenv = require("dotenv");
 const router = require("./routes/todo.router.js");
 
 dotenv.config();
+
 const app = express();
 const port = process.env.PORT || 8000;
 
 app.set("view engine", "ejs");
-app.set("views", "./views");
-
-// app.use(express.json());
+app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-app.use('/', router)
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/", router);
 
 app.get("/", (req, res) => {
-  res.send({
+  res.json({
     backend: "Healthy",
-    port: port,
+    port,
   });
 });
 
-app.listen(port, () => {
-  console.log(`Backend running on port: ${port}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  app.listen(port, () => {
+    console.log(`Backend running on port: ${port}`);
+  });
+}
+
+module.exports = app;
